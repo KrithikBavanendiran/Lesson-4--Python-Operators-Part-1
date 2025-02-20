f1778bd5ba46a4385bcb0bad23ab2124c35807c2
KrithikBavanendiran <bvino1522@gmail.com>
2025-02-20 18:22:40 +0000

    Averages calculator

---
+++ Averages calculator.py
@@ -0,0 +1,11 @@
+Number1=98
+Number2=234
+Number3=938
+Number4=232
+Number5=90
+print("We will find the average of 98, 234, 938, 232 and 90")
+sum=Number1+Number2+Number3+Number4+Number5
+print("calculating...")
+average=sum/5
+print("Here is the average of these numbers:")
+print(average)
