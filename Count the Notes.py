a602c0fba53effe2437f8bc95458aa7d0633e5c7
KrithikBavanendiran <bvino1522@gmail.com>
2025-02-20 18:34:25 +0000

    Counting the Notes

---
+++ Count the Notes.py
@@ -0,0 +1,12 @@
+Amount=int(input("Enter amount of withdrawal:"))
+note1=Amount//1000
+note2=Amount//100
+note3=Amount//50
+note4=Amount//20
+note5=Amount//10
+
+print("Notes of 1000 rupees",note1)
+print("Notes of 100 rupees",note2)
+print("Notes of 50 rupees",note3)
+print("Notes of 20 rupees",note4)
+print("Notes of 10 rupees",note5)
