d26e4d08db98aefd6acefb0cfad6fb157d2a5e66
KrithikBavanendiran <bvino1522@gmail.com>
2025-02-20 18:41:31 +0000

    Percentage Calculation

---
+++ Percentage Calculation.py
@@ -0,0 +1,9 @@
+print("We will calculate tghe average percentage of Raj's test scores:")
+Maths=40
+Science=70
+Hindi=50
+English=60
+sum=Maths+Science+Hindi+English
+average=sum/4
+print("calculating...")
+print("Here is Raj's average percentage, he scored", average)
